3adc86dc3ac679e435d4cd9443484fb1fcbf3c3b
Kalter <kalterxad34@gmail.com>
2017-08-10 19:37:24 +0300

    add to eer diagram table with stop words

--- routes/user/profile.js
+++ routes/user/profile.js
@@ -19,11 +19,44 @@ module.exports = (app, passport, isLoggedIn, User, Note) => {
                 });
         });
     });
+
     app.get('/settings', isLoggedIn, (req, res) => {
         res.render('user/settings', {
             user: req.user,
             successMessage: req.flash('successMessage'),
             errorMessage: req.flash('errorMessage')
+        });
+    });
+
+    app.post('/settings', isLoggedIn, (req, res) => {
+        if (!req.body.pass && !req.body.username) {
+            req.flash('error-message', 'Хотя бы одно поле должно быть заполненным!');
+            return res.redirect('/settings');
+        }
+        const newInfo = {
+            id: req.user.id,
+            username: req.body.username,
+            pass: req.body.pass,
+            admin: req.user.admin
+        };
+
+        User.update(newInfo, err => {
+            if (err){
+                req.flash('errorMessage', 'try again \r\n' + err);
+                return res.redirect('/settings');
+            }
+            req.flash('successMessage', 'Successfully updated!');
+            res.redirect('/settings');
+        })
+    });
+
+    app.post('/deleteuser', isLoggedIn, (req, res) => {
+        User.delete(req.user, (err) => {
+            if (err) {
+                req.flash('errorMessage', 'Sorry, try again later. Error:' + err);
+                res.redirect('/settings');
+            }
+            res.redirect('/logout');
         });
     });
 };
